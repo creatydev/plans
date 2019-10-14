1a727153e85dcbd7b29c94082aff41152f5b5724
Dukens Thelemaque <dukenst2006@gmail.com>
2019-10-13 22:04:51 -0400

    Replace depreciate str_random by facade Str

--- database/factories/PlanFactory.php
+++ database/factories/PlanFactory.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -12,7 +13,7 @@
 
 $factory->define(\Rennokki\Plans\Models\PlanModel::class, function () {
     return [
-        'name' => 'Testing Plan '.str_random(7),
+        'name' => 'Testing Plan '.Str::random(7),
         'description' => 'This is a testing plan.',
         'price' => (float) mt_rand(10, 200),
         'currency' => 'EUR',
